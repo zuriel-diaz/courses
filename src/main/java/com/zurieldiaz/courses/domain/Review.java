daf03bc880892793372a4aabd7320fa9e62eb2c1
Zuriel <xthr3mx@gmail.com>
2019-05-16 18:51:12 -0500

    Unused import removed.

--- src/main/java/com/zurieldiaz/courses/domain/Review.java
+++ src/main/java/com/zurieldiaz/courses/domain/Review.java
@@ -1,7 +1,6 @@
 package com.zurieldiaz.courses.domain;
 
 import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +11,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
